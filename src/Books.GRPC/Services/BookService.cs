99f3c402c994e999b2ab6b3ec8e6d10d306a95b9
Tarek Mostafa <tarek.r.mostafa@gmail.com>
2024-12-27 20:01:10 +0200

    implement book service

--- src/Books.GRPC/Services/BookService.cs
+++ src/Books.GRPC/Services/BookService.cs
@@ -1,36 +1,112 @@
-﻿using Grpc.Core;
+﻿using Books.GRPC.DbContext;
+using Grpc.Core;
+using Microsoft.EntityFrameworkCore;
 
 namespace Books.GRPC.Services;
 
 public class BookService: GRPC.BookService.BookServiceBase
 {
-	public BookService()
+	private readonly BooksContext _booksContext;
+
+	public BookService(BooksContext booksContext)
 	{
-		
+		_booksContext = booksContext;
 	}
 
-	public override Task<Book> GetBook(GetBookRequest request, ServerCallContext context)
+	public override async Task<BookUnit> GetBook(GetBookRequest request, ServerCallContext context)
 	{
-		return base.GetBook(request, context);
+		// Get the book from the database
+		var book = await _booksContext.Books.FindAsync(request.Id);
+		if (book == null)
+		{
+			throw new RpcException(new Status(StatusCode.NotFound, "Book not found"));
+		}
+		// return the book
+		return new BookUnit
+		{
+			Id = book.Id,
+			Title = book.Title,
+			Author = book.Author,
+			Description = book.Description,
+			Image = book.Image
+		};
 	}
 
-	public override Task GetBooks(GetBooksRequest request, IServerStreamWriter<Book> responseStream, ServerCallContext context)
+	public override async Task GetBooks(GetBooksRequest request, IServerStreamWriter<BookUnit> responseStream, ServerCallContext context)
 	{
-		return base.GetBooks(request, responseStream, context);
+		// Get all the books from the database
+		var books = await _booksContext.Books.AsNoTracking().ToListAsync();
+		foreach (var book in books)
+		{
+			await responseStream.WriteAsync(new BookUnit
+			{
+				Id = book.Id,
+				Title = book.Title,
+				Author = book.Author,
+				Description = book.Description,
+				Image = book.Image
+			});
+		}
+
 	}
 
-	public override Task<AddBookResponse> AddBook(AddBookRequest request, ServerCallContext context)
+	public override async Task<AddBookResponse> AddBook(AddBookRequest request, ServerCallContext context)
 	{
-		return base.AddBook(request, context);
+		// Add the book to the database
+		var book = new Book
+		{
+			Title = request.Title,
+			Author = request.Author,
+			Description = request.Description,
+			Image = request.Image
+		};
+		_booksContext.Books.Add(book);
+		await _booksContext.SaveChangesAsync();
+		// return the response
+		return new AddBookResponse
+		{
+			Id = book.Id
+		};
 	}
 
-	public override Task<UpdateBookResponse> UpdateBook(UpdateBookRequest request, ServerCallContext context)
+	public override async Task<UpdateBookResponse> UpdateBook(UpdateBookRequest request, ServerCallContext context)
 	{
-		return base.UpdateBook(request, context);
+		// Get the book from the database
+		var book = await _booksContext.Books.FindAsync(request.Id);
+		if (book == null)
+		{
+			throw new RpcException(new Status(StatusCode.NotFound, "Book not found"));
+		}
+		// Update the book
+		book.Title = request.Title;
+		book.Author = request.Author;
+		book.Description = request.Description;
+		book.Image = request.Image;
+		await _booksContext.SaveChangesAsync();
+		// return the response
+		return new UpdateBookResponse
+		{
+			Success = true
+		};
+
 	}
 
-	public override Task<DeleteBookResponse> DeleteBook(DeleteBookRequest request, ServerCallContext context)
+	public override async Task<DeleteBookResponse> DeleteBook(DeleteBookRequest request, ServerCallContext context)
 	{
-		return base.DeleteBook(request, context);
+		// Get the book from the database
+		var book = await _booksContext.Books.FindAsync(request.Id);
+		if (book == null)
+		{
+			throw new RpcException(new Status(StatusCode.NotFound, "Book not found"));
+		}
+		// Delete the book
+		_booksContext.Books.Remove(book);
+		await _booksContext.SaveChangesAsync();
+		// return the response
+		return new DeleteBookResponse
+		{
+			Success = true
+		};
+
 	}
 }
